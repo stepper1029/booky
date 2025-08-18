5c8815f6e53b505d2bc3fd206990cb91e8edc74a
stepper1029 <olivian.stepper@gmail.com>
2025-08-18 15:53:16 -0400

    Initial spring boot setup

---
+++ src/main/java/com/example/stepperlibrary/StepperLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.stepperlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepperLibraryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StepperLibraryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/stepperlibrary/StepperLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepperlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepperLibraryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
